ca092a3d76058bf6fdd1b7c7b62d0e45a3677baa
crassSandwich <christopherjanidlo@gmail.com>
2019-01-18 21:26:51 -0500

    Player Movement

---
+++ Assets/Scripts/DroneMovement.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DroneMovement : MonoBehaviour
+{
+    public float MoveSpeed;
+    public float JumpForce;
+
+    public LayerMask GroundLayers;
+    public float HalfHeight;
+    public Vector3 GroundCheckHalfExtents;
+
+    Rigidbody rb;
+    bool canJump = true;
+
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update ()
+    {
+        Vector3 movement = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized * MoveSpeed;
+
+        movement = transform.TransformDirection(movement);
+
+        rb.velocity = new Vector3(movement.x, rb.velocity.y, movement.z);
+
+        if (grounded())
+        {
+            if (canJump && Input.GetButton("Jump"))
+            {
+                jump();
+            }
+        }
+        else
+        {
+            canJump = true;
+        }
+    }
+
+    bool grounded ()
+    {
+        return Physics.BoxCast(transform.position, GroundCheckHalfExtents, Vector3.down, transform.rotation, HalfHeight, GroundLayers, QueryTriggerInteraction.Ignore);
+    }
+
+    void jump ()
+    {
+        rb.AddForce(Vector3.up * JumpForce, ForceMode.Impulse);
+        canJump = false;
+    }
+}
