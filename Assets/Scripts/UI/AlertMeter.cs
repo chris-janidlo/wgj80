92d0ee72200babfd8eb3b6aea718565a8ceee5cb
crassSandwich <christopherjanidlo@gmail.com>
2019-01-21 01:14:53 -0500

    Alert meter

---
+++ Assets/Scripts/UI/AlertMeter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class AlertMeter : MonoBehaviour
+{
+    Image image;
+
+    void Start ()
+    {
+        image = GetComponent<Image>();
+    }
+
+    void Update ()
+    {
+        image.fillAmount = SecurityManager.Instance.Alarm / 100;
+    }
+}
